abff3221059e22b0b041e5fa0583093e170f3234
sumaiyaansarihere <sumaiyaansari423@gmail.com>
2024-10-30 15:01:22 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,32 @@
+import inquirer from "inquirer"
+let todos:string[] = []
+let condition = true;
+while(condition){
+    const addtask :{ Todo:string, addMore:boolean}  = await inquirer.prompt([
+            {   
+                name:"Todo",
+                type:"input",
+                message: "What do you wanna add in your todos?"
+            },
+            {
+                name:"addMore",
+                type:"confirm",
+                message:"Do you wanna add more todos",
+                default:"false"
+            }]);
+    const {addMore, Todo} = addtask;
+    console.log(addtask)
+    condition = addMore
+    if(Todo){
+        todos.push(Todo)
+    }else{
+        console.log("add valid input")
+    }
+    if (todos. length >0){
+        console.log("your Todo list: ")
+        todos.forEach(todo => {
+            console.log(todo)
+        });
+    }else {
+        console.log("Not Todos Found") 
+    }}
